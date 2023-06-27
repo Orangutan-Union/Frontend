a1f279fcc39872740fb3ac94fa2ae50a624c4056
jwn <jwn@PROELE10>
2023-06-27 10:36:05 +0200

    set up friendlist

--- src/app/components/FriendsWithBenefits/friendlist/friendlist.component.ts
+++ src/app/components/FriendsWithBenefits/friendlist/friendlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendFollower } from 'src/app/models/friendfollower';
+import { FriendfollowerService } from 'src/app/services/friendfollower.service';
 
 @Component({
   selector: 'app-friendlist',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendlistComponent implements OnInit {
 
-  constructor() { }
+  friends: FriendFollower[] = [];
+  constructor(private ffService: FriendfollowerService) { }
 
   ngOnInit(): void {
+    this.getUserFriends();
+  }
+
+  getUserFriends(){
+    let userId = Number(localStorage.getItem('userid'));
+    this.ffService.getUserFriends(userId).subscribe(res => {
+      this.friends = res;
+      console.log(this.friends);
+    });
   }
 
 }
