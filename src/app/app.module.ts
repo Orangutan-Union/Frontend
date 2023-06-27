8fa2a622f759083e4e85067490ffabe38c01b917
Jeppe <88774896+HildaTipsyToe@users.noreply.github.com>
2023-06-27 10:28:05 +0200

    LikeDislike not full page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,8 +31,9 @@ import { FriendrequestsComponent } from './components/FriendsWithBenefits/friend
 import { CommentsComponent } from './components/feeds/comments/comments.component';
 import { PostHeaderComponent } from './components/feeds/Post-Feed-componentsFolder/post-header/post-header.component';
 import { FeedComponentComponent } from './components/feeds/Post-Feed-componentsFolder/post-feed-component/feed-component.component';
+import { PostToolbarComponent } from './components/feeds/Post-Feed-componentsFolder/post-toolbar/post-toolbar.component';
 
-
+// ./components/feeds/post-feed-componentsFolder/post-toolbar/post-toolbar.component
 export function tokenGetter() {
   return localStorage.getItem("jwt");
 }
@@ -62,6 +63,7 @@ export function tokenGetter() {
     FriendrequestsComponent,
     PostHeaderComponent,
     FeedComponentComponent,
+    PostToolbarComponent,
   ],
   imports: [
     BrowserModule,
