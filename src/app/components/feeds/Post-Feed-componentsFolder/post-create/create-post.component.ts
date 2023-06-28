0270081fc68ad8893b2f7730310aaeb81200baea
jwn <jwn@PROELE10>
2023-06-28 13:33:23 +0200

    upload image using formdata

--- src/app/components/feeds/Post-Feed-componentsFolder/post-create/create-post.component.ts
+++ src/app/components/feeds/Post-Feed-componentsFolder/post-create/create-post.component.ts
@@ -9,6 +9,8 @@ import { FeedService } from 'src/app/services/feed.service';
 })
 export class CreatePostComponent implements OnInit {
   post: NewPost = new NewPost
+  formData = new FormData();
+  file: any;
   @Input() posts: any[] = [];
   @Input() commentCounter: number[] = [];
   @Input() likeCounter: number[] = [];
@@ -21,16 +23,43 @@ export class CreatePostComponent implements OnInit {
 
   onSubmit(): void {
     this.post.userId = Number(localStorage.getItem('userid'));
+    // this.formData.append('post', JSON.stringify(this.post));
+    this.formData.append('user_id', this.post.userId.toString());
+    this.formData.append('content', this.post.content);
+    this.formData.append('longitude', this.post.longtitude.toString());
+    this.formData.append('latitude', this.post.latitude.toString());
+    if(this.post.groupId !== null){
+      this.formData.append('group_id', this.post.groupId!.toString());
+    }
+    else{
+      this.formData.append('group_id', '');
+    }
 
-    this.feedService.addPost(this.post).subscribe(newPost => {
+    this.feedService.addPost(this.formData).subscribe(newPost => {
       this.feedService.getFullPost(newPost.postId).subscribe(newPost => {
         this.posts.unshift(newPost);
         this.commentCounter.unshift(0);
         this.likeCounter.unshift(0);
         this.dislikeCounter.unshift(0);
+        this.formData.delete('file');
+        this.formData.delete('filename');
+        this.formData.delete('post');
+        this.formData.delete('content');
+        this.formData.delete('longitude');
+        this.formData.delete('longitude');
+        this.formData.delete('group_id');
+        this.file = undefined
       })
     })
 
     this.post.content = '';
+  }
+
+  fileChange(files: any) {
+    if (files && files.length > 0) {
+      this.file = files[0];
+      this.formData.append('file', this.file);
+      this.formData.append('filename', this.file.name);      
+    }
   }
 }
