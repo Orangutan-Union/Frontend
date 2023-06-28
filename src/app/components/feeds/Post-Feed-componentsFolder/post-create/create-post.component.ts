7d0e572769e69ce13b9946bb5744c3eeca925030
jwn <jwn@PROELE10>
2023-06-28 14:22:10 +0200

    add post pictures to feeds moved formData cleaning to its own function

--- src/app/components/feeds/Post-Feed-componentsFolder/post-create/create-post.component.ts
+++ src/app/components/feeds/Post-Feed-componentsFolder/post-create/create-post.component.ts
@@ -18,20 +18,19 @@ export class CreatePostComponent implements OnInit {
   constructor(private feedService: FeedService) { }
 
   ngOnInit(): void {
-    console.log(this.posts);    
+    console.log(this.posts);
   }
 
   onSubmit(): void {
     this.post.userId = Number(localStorage.getItem('userid'));
-    // this.formData.append('post', JSON.stringify(this.post));
     this.formData.append('user_id', this.post.userId.toString());
     this.formData.append('content', this.post.content);
     this.formData.append('longitude', this.post.longtitude.toString());
     this.formData.append('latitude', this.post.latitude.toString());
-    if(this.post.groupId !== null){
+    if (this.post.groupId !== null) {
       this.formData.append('group_id', this.post.groupId!.toString());
     }
-    else{
+    else {
       this.formData.append('group_id', '');
     }
 
@@ -41,14 +40,7 @@ export class CreatePostComponent implements OnInit {
         this.commentCounter.unshift(0);
         this.likeCounter.unshift(0);
         this.dislikeCounter.unshift(0);
-        this.formData.delete('file');
-        this.formData.delete('filename');
-        this.formData.delete('post');
-        this.formData.delete('content');
-        this.formData.delete('longitude');
-        this.formData.delete('longitude');
-        this.formData.delete('group_id');
-        this.file = undefined
+        this.cleanFormData();
       })
     })
 
@@ -59,7 +51,18 @@ export class CreatePostComponent implements OnInit {
     if (files && files.length > 0) {
       this.file = files[0];
       this.formData.append('file', this.file);
-      this.formData.append('filename', this.file.name);      
+      this.formData.append('filename', this.file.name);
     }
+  }
+
+  cleanFormData() {
+    this.file = undefined;
+    this.formData.delete('file');
+    this.formData.delete('filename');
+    this.formData.delete('post');
+    this.formData.delete('content');
+    this.formData.delete('longitude');
+    this.formData.delete('longitude');
+    this.formData.delete('group_id');
   }
 }
