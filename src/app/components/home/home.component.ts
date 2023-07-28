7fb036e2806b2e04965714c57f409d81c7e46f2d
jwn <jwn@PROELE10>
2023-07-28 08:52:46 +0200

    load user and check for received friendrequests on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { ChangePassword } from 'src/app/models/changepassword';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { FriendrequestService } from 'src/app/services/friendrequest.service';
 
 @Component({
   selector: 'app-home',
@@ -19,15 +20,25 @@ export class HomeComponent implements OnInit {
   user: User = new User;
   userId: number = 0;
   formData = new FormData();
-  constructor(private authService: AuthService, private route: Router) { }
+  constructor(private authService: AuthService, private route: Router, private frService: FriendrequestService) { }
 
   ngOnInit(): void {   
    let FriendsBoolean = localStorage.getItem('FriendBoolean')
    let FollowerBoolean = localStorage.getItem('FollowerBoolean')
    this.FollowerBoolean = JSON.parse(FollowerBoolean!)
    this.FriendsBoolean = JSON.parse(FriendsBoolean!)
+   this.getUser();
 }
 
+  getUser(){
+    this.authService.getUserById(Number(localStorage.getItem('userid'))).subscribe(res => {
+      this.user = res;
+      //If there are any received friendrequests pending then show the notification indicator
+      if (this.user.receivedFriendRequests.length > 0) {
+        this.frService.updateNotificationDot(true);
+      }      
+    });
+  }
   
   FriendsFeed(){
     this.FriendsBoolean = !this.FriendsBoolean
